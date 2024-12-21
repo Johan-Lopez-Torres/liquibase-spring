212bfe3ad4594957b98f6b5170abc2914dfaf765
Your Name <you@example.com>
2024-12-21 17:30:54 -0500

    changes

---
+++ src/main/java/com/johan/liquibasespring/LiquibaseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.johan.liquibasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/johan/liquibasespring/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.johan.liquibasespring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> { }
